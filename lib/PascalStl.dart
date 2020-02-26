8a6f60ad57361d8bca3f36e5dcfc5316e04f82e2
Raymond Bayly <60951450+raymondbayly@users.noreply.github.com>
2020-02-26 15:00:11 -0500

    update code on flutter

---
+++ lib/PascalStl.dart
@@ -0,0 +1,27 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return MaterialApp(
+      title: 'Rays Base Fluter App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome | Rays Flutter test'),
+        ),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
+        ),
+      ),
+    );
+  }
+}
+
